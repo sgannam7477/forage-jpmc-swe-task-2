f1eab05b8c3ecb3e5aba84e4540abaf64302d218
shrenick <sgannam@purdue.edu>
2024-01-21 22:32:51 -0500

    Task 2 - Updated

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ class App extends Component<{}, IState> {
         // Update the state by creating a new array of data that consists of
         // Previous data in the state and the new data from server
         this.setState({
-        data: serverResponds,
+        data: [...serverResponds, ...this.state.data],
         showGraph: true,
         });
       });
